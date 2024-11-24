07b5b0262168bc0336bb2ebb52b62a2732efe470
tomfaical <tomfaicalr@gmail.com>
2024-11-24 17:59:57 -0300

    update structure

--- NHPTplusFlask/app.py
+++ NHPTplusFlask/app.py
@@ -17,7 +17,7 @@ def home():
     if request.method == "POST":
         paciente_nome = request.form.get("nome_paciente")
         return redirect(url_for("coleta"))
-    return render_template("base.html")
+    return render_template("[0] base.html")
 
 # Rota para a tela de Coleta
 @app.route("/coleta")
@@ -25,7 +25,7 @@ def coleta():
     global paciente_nome, n_coleta_ruim, n_coleta_bom
     data_atual = datetime.now().strftime("%d/%m/%Y")
     return render_template(
-        "coleta.html",
+        "[1.0] coleta.html",
         paciente_nome=paciente_nome,
         data_atual=data_atual,
         n_coleta_ruim=n_coleta_ruim,
@@ -36,42 +36,42 @@ def coleta():
 @app.route("/coleta/paretico")
 def coleta_paretico():
     global paciente_nome
-    return render_template("paretico.html", paciente_nome=paciente_nome)
+    return render_template("[1.1] paretico.html", paciente_nome=paciente_nome)
 
 @app.route("/incrementar_paretico", methods=["POST"])
 def incrementar_paretico():
     global n_coleta_ruim
     n_coleta_ruim += 1
-    return redirect(url_for("coleta"))
+    return redirect(url_for("[1.0] coleta"))
 
 # Rota para a tela de Coleta do braço saudável
 @app.route("/coleta/saudavel")
 def coleta_saudavel():
     global paciente_nome
-    return render_template("saudavel.html", paciente_nome=paciente_nome)
+    return render_template("[1.2] saudavel.html", paciente_nome=paciente_nome)
 
 @app.route("/incrementar_saudavel", methods=["POST"])
 def incrementar_saudavel():
     global n_coleta_bom
     n_coleta_bom += 1
-    return redirect(url_for("coleta"))
+    return redirect(url_for("[1.0] coleta"))
 
 
 # Rotas para os outros botões do header
 @app.route("/graficos")
 def graficos():
     global paciente_nome
-    return render_template("graficos.html", paciente_nome=paciente_nome)
+    return render_template("[2.0] graficos.html", paciente_nome=paciente_nome)
 
 @app.route("/tabelas")
 def tabelas():
     global paciente_nome
-    return render_template("tabelas.html", paciente_nome=paciente_nome)
+    return render_template("[3.0] tabelas.html", paciente_nome=paciente_nome)
 
 @app.route("/area-dev")
 def area_dev():
     global paciente_nome
-    return render_template("area_dev.html", paciente_nome=paciente_nome)
+    return render_template("[4.0] area_dev.html", paciente_nome=paciente_nome)
 
 # Iniciar o servidor Flask
 def iniciar_servidor():
